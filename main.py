1ebaf53f8f9de851ad86f720fa1f98f719a9bb27
mdiannna <marusicdiana@gmail.com>
2025-08-13 10:46:58 +0200

    implementing the frontend without RAG

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ from langchain_init import init_llm_langchain
 import uvicorn
 
 
-llm, embeddings = init_llm_and_embeddings()
-vector_store = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+# TODO: enable after testing the frontend
+# llm, embeddings = init_llm_and_embeddings()
+# vector_store = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
 
-texts = ["AI agents are autonomous decision-making systems.", "Vector databases help store and retrieve embeddings efficiently."]
-vector_store.add_texts(texts)
+# texts = ["AI agents are autonomous decision-making systems.", "Vector databases help store and retrieve embeddings efficiently."]
+# vector_store.add_texts(texts)
 
 
-chain = init_llm_langchain(llm)
+# chain = init_llm_langchain(llm)
 
 app = FastAPI()
 templates = Jinja2Templates("templates")
@@ -32,13 +33,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 async def read_root(request:Request):
     return templates.TemplateResponse("index.html", {"request":request})
 
-
-@app.get("/ask")
-def ask_agent(question: str):
-    relevant_docs = vector_store.similarity_search(question, k=1)
-    context = " ".join([doc.page_content for doc in relevant_docs])
-    response = chain.run(question=question + " " + context)
-    return {"response": response}
+# TODO: enable after testing the frontend and make POST request
+# @app.get("/ask")
+# def ask_agent(question: str):
+#     relevant_docs = vector_store.similarity_search(question, k=1)
+#     context = " ".join([doc.page_content for doc in relevant_docs])
+#     response = chain.run(question=question + " " + context)
+#     return {"response": response}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
