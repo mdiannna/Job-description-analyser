ce7d30d4abc55c1c03e36e723ca1b96746eee2c2
mdiannna <marusicdiana@gmail.com>
2025-08-16 17:53:00 +0200

    added initial simulation of a conversation in the chat, without llm

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from langchain_community.vectorstores import Chroma
 
 
 from fastapi import FastAPI
+from typing import Annotated
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi import Request, HTTPException
-from fastapi import File, UploadFile
+from fastapi import File, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
 
 from llm_init import init_llm_and_embeddings
@@ -19,14 +20,14 @@ import shutil
 from text_processing import read_pdf, tokenize_pdf_text
 
 # TODO: enable after testing the frontend
-llm, embeddings = init_llm_and_embeddings()
-vector_store = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+# llm, embeddings = init_llm_and_embeddings()
+# vector_store = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
 
-texts = ["AI agents are autonomous decision-making systems.", "Vector databases help store and retrieve embeddings efficiently."]
-vector_store.add_texts(texts)
+# texts = ["AI agents are autonomous decision-making systems.", "Vector databases help store and retrieve embeddings efficiently."]
+# vector_store.add_texts(texts)
 
 
-chain = init_llm_langchain(llm)
+# chain = init_llm_langchain(llm)
 
 app = FastAPI()
 templates = Jinja2Templates("templates")
@@ -75,18 +76,30 @@ async def process_file():
     pages = await read_pdf(jd_filename)
     splits_pages = tokenize_pdf_text(pages)
     print("type splits pages:", type(splits_pages[0]))
-    _ = vector_store.add_documents(documents=splits_pages)
+    # TODO: enable after testing the frontend and make POST request
+    # _ = vector_store.add_documents(documents=splits_pages)
 
     return {"status":"success", "nr_of_pages":len(pages)} #TODO: check if succesful
 
 
 # TODO: enable after testing the frontend and make POST request
-@app.get("/ask")
-def ask_agent(question: str):
-    relevant_docs = vector_store.similarity_search(question, k=1)
-    context = " ".join([doc.page_content for doc in relevant_docs])
-    response = chain.run(question=question + " " + context)
-    return {"response": response}
+# @app.get("/ask")
+# def ask_agent(question: str):
+#     relevant_docs = vector_store.similarity_search(question, k=1)
+#     context = " ".join([doc.page_content for doc in relevant_docs])
+#     response = chain.run(question=question + " " + context)
+#     return {"response": response}
+
+@app.post("/ask")
+def ask_agent(message: Annotated[str, Form()]):
+    question = message
+    print("question:", question)
+    response = "test test"
+    # relevant_docs = vector_store.similarity_search(question, k=1)
+    # context = " ".join([doc.page_content for doc in relevant_docs])
+    # response = chain.run(question=question + " " + context)
+    return {"status":"success", "answer": response}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
